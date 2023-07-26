f996d293fc1517a39f8a4a2910b7a6b93c2082e9
Wdidwiky <alexferguson.af3@gmail.com>
2023-07-26 16:53:48 +0700

    login

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LoginController;
 use App\Models\style;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StyleController;
 use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\TrendController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,15 +30,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/style', [StyleController::class, 'view']);
 Route::post('/style' ,[StyleController::class, 'store']);
 Route::post('/product' ,[ProductController::class, 'storetodata']);
-Route::get('/product/index' ,[ProductController::class, 'index']);
+//Route::get('/product' ,[ProductController::class, 'index']);
 Route::get('/product/{id}' ,[ProductController::class, 'showproduct']);
 Route::put('/updateproduct/{id}', [ProductController::class, 'updatecomponent']);
 Route::post('/perbarui/gambar/{id}', [ProductController::class, 'perbarui']);
 Route::delete('/product/delete/{id}', [ProductController::class, 'delete']);
 Route::get('/style/{id}' ,[StyleController::class, 'showing']);
 Route::get('/style/{id}', [StyleController::class, 'showproductid']);
-Route::get('/product', [ProductController::class, 'index']); // ini buat yang nampilin semua product
+//Route::get('/product', [ProductController::class, 'index']); // ini buat yang nampilin semua product
 Route::get('/style/product/{id}', [ProductController::class, 'showproductid']); // ini buat nampilin product dengan id style
 Route::get('/products/{style_id}', [ProductController::class, 'showproductid']); // ini nampilin product berdasarkan idnya
+Route::post('/signup', [UserController::class, 'signup']);
+Route::get('/signup/google', [UserController::class, 'redirectToGoogle']);
+Route::get('/signup/google/callback', [UserController::class,'handleGoogleCallback']);
+Route::post('/login', [userController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/login/google', [UserController::class, 'redirectToGoogle']);
+
 
 
